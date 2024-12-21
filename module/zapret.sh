46f1fd02219343d47dfad3ecb3498d8f42b6e0b1
Seva <68725282+sevcator@users.noreply.github.com>
2024-12-21 11:58:14 +0700

    Update zapret.sh; check if nfqws is runned already and try to fix nfqws

--- module/zapret.sh
+++ module/zapret.sh
@@ -7,16 +7,14 @@ boot_wait() {
 boot_wait
 
 MODDIR=/data/adb/modules/zapret
-config="--filter-tcp=80,443 --hostlist-exclude=$MODDIR/exclude.txt --hostlist-auto=$MODDIR/autohostlist.txt --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODDIR/t_google.bin --dpi-desync-fake-quic=$MODDIR/q_google.bin --new";
+config="--filter-tcp=80,443 --hostlist-exclude=$MODDIR/exclude.txt --hostlist-auto=$MODDIR/autohostlist.txt --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODDIR/t_google.bin --dpi-desync-fake-quic=$MODDIR/q_google.bin --new";
 config="$config --filter-udp=50000-50099 --ipset=$MODDIR/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-fake-tls=$MODDIR/t_google.bin --dpi-desync-fake-quic=$MODDIR/q_google.bin --new";
 config="$config --filter-udp=80,443 --hostlist-exclude=$MODDIR/exclude.txt --hostlist-auto=$MODDIR/autohostlist.txt --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-tls=$MODDIR/t_google.bin --dpi-desync-fake-quic=$MODDIR/q_google.bin";
 
 sysctl net.netfilter.nf_conntrack_tcp_be_liberal=1 > /dev/null;
-
 chmod 755 $MODDIR/*
 chmod 666 "$MODDIR"/*.txt
 chmod 666 "$MODDIR"/*.bin
-
 cbOrig="-m connbytes --connbytes-dir=original --connbytes-mode=packets --connbytes 1:12 -m mark ! --mark 0x40000000/0x40000000";
 cbReply="-m connbytes --connbytes-dir=reply --connbytes-mode=packets --connbytes 1:6 -m mark ! --mark 0x40000000/0x40000000";
 
@@ -54,10 +52,22 @@ udp_ports="$(echo $config | grep -oE 'filter-udp=[0-9,-]+' | sed -e 's/.*=//g' -
 iptMultiPort "tcp" "$tcp_ports";
 iptMultiPort "udp" "$udp_ports";
 
+check_nfqws_running() {
+    if pgrep -x "nfqws" > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if check_nfqws_running; then
+    exit 0
+fi
+
 while true; do
     if ! pgrep -x "nfqws" > /dev/null; then
 	   # echo "[$(date)] nfqws not started, restarting..." >> "$MODDIR/logs.txt"
-	   "$MODDIR/nfqws" --uid=0:0 --qnum=200 $config > /dev/null &
+	   "$MODDIR/nfqws" --uid=0:0 --bind-fix4 --qnum=200 $config > /dev/null &
 	   # echo "[$(date)] nfqws - PID $NFQWS_PID" >> "$MODDIR/logs_watchdog.txt"
     fi
     if ! iptables -t mangle -L POSTROUTING | grep -q "NFQUEUE"; then

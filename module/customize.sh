126b3c16b0874518bd7c799e81ec08519c329e35
Seva <68725282+sevcator@users.noreply.github.com>
2024-12-24 14:39:18 +0700

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -22,26 +22,26 @@ check_requirements() {
       BINARY=nfqws-x86_x64
       ;;
     *)
-      ui_print "! Unsupported architecture: $ARCH"
+      ui_print "! Device Architecture: $ARCH"
       abort
       ;;
   esac
 
   if [ -n "$API" ]; then
-    ui_print "- Device Android API is $API"
+    ui_print "- Device Android API: $API"
     if [ "$API" -lt 27 ]; then
-      ui_print "! Minimal Android API level is required: 27 (Android 7.1)"
+      ui_print "! Device Android API: required 27 (Android 7.1)"
       abort
     fi
   else
-    ui_print "! Unable to determine Android API level"
+    ui_print "! Device Android API: Error"
     exit 1
   fi
 
   if which busybox > /dev/null 2>&1; then
-    ui_print "- The device has busybox installed"
+    ui_print "- Busybox: Installed"
   else
-    ui_print "! busybox is not installed on the device!"
+    ui_print "! Busybox: Not found"
     abort
   fi
 }
@@ -50,32 +50,26 @@ check_requirements
 
 install_module() {
   MODULE_UPDATE_DIR="/data/adb/modules_update/zapret"
-  
-  ui_print "- Killing processes"
+  MODULE_DIR="/data/adb/modules/zapret"
   pkill nfqws
   pkill zapret
 
-  ui_print "- Copying nfqws for $ARCH"
-  mv "$MODULE_UPDATE_DIR/$BINARY" "$MODPATH/nfqws"
-  mv "$MODPATH/$BINARY" "$MODPATH/nfqws"
-
-  ui_print "- Removing binaries for another processors"
-  rm -rf $MODPATH/nfqws-*
+  ui_print "- Device Architecture: $ARCH"
+  mv "$MODULE_UPDATE_DIR/$BINARY" "$MODULE_DIR/nfqws"
+  mv "$MODULE_DIR/$BINARY" "$MODULE_DIR/nfqws"
+  rm -rf $MODULE_DIR/nfqws-*
   rm -rf $MODULE_UPDATE_DIR/nfqws-*
 
-  if ls $MODPATH/*.txt 1> /dev/null 2>&1; then
-    ui_print "- Copying txt in update dir"
-    cp $MODPATH/*.txt $MODULE_UPDATE_DIR/
+  if ls $MODULE_DIR/*.txt 1> /dev/null 2>&1; then
+    cp $MODULE_DIR/*.txt $MODULE_UPDATE_DIR/
   fi
 
-  ui_print "- Fixing scripts"
-  sed -i 's/\r$//' "$MODPATH/service.sh"
-  sed -i 's/\r$//' "$MODPATH/zapret-service"
-  sed -i 's/\r$//' "$MODPATH/uninstall.sh"
+  sed -i 's/\r$//' "$MODULE_DIR/service.sh"
+  sed -i 's/\r$//' "$MODULE_DIR/zapret-service"
+  sed -i 's/\r$//' "$MODULE_DIR/uninstall.sh"
   
   ui_print "- Setting permissions"
-  chmod 755 "$MODPATH"/*
-  chmod 755 "$SERVICE_DIR/zapret.sh"
+  chmod 755 "$MODULE_DIR"/*
 
   ui_print "*******************************************************"
   ui_print "-         sevcator.t.me / sevcator.github.io           "

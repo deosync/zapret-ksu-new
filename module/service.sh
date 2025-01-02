dca5c3221b4e01d022b8b72034a30afd5aaa6a9a
senko.class <79597112+deosync@users.noreply.github.com>
2025-01-02 23:22:20 +0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,3 +1,28 @@
 #!/system/bin/sh
-
+case "$ARCH" in
+    arm)
+      BINARY=nfqws-arm
+      ;;
+    arm64)
+      BINARY=nfqws-aarch64
+      ;;
+    x86)
+      BINARY=nfqws-x86
+      ;;
+    x86_64)
+      BINARY=nfqws-x86_x64
+      ;;
+    *)
+      ui_print "! Device Architecture: $ARCH"
+      abort
+      ;;
+esac
+if [ -f "/data/adb/modules/zapret/$BINARY" ]; then
+    mv "/data/adb/modules/zapret/$BINARY" "/data/adb/modules/zapret/nfqws"
+    chmod 777 "/data/adb/modules/zapret/nfqws"
+    rm -rf /data/adb/modules/zapret/nfqws-*
+    if [ $(stat -c "%a" "/data/adb/modules/zapret/zapret.sh") != "777" ]; then
+      chmod 777 "/data/adb/modules/zapret/zapret.sh"
+    fi
+fi
 su -c "/data/adb/modules/zapret/zapret.sh"

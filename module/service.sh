42e735c24f46238e027d17c62afa9c00b9db8fd9
senko.class <79597112+deosync@users.noreply.github.com>
2025-01-02 23:32:14 +0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-case "$ARCH" in arm) BINARY=nfqws-arm;; arm64) BINARY=nfqws-aarch64;; x86) BINARY=nfqws-x86;; x86_64) BINARY=nfqws-x86_x64;; *) esac
+case "$ARCH" in arm) BINARY=nfqws-arm;; arm64) BINARY=nfqws-aarch64;; x86) BINARY=nfqws-x86;; x86_64) BINARY=nfqws-x86_x64) esac
 if [ -f "/data/adb/modules/zapret/$BINARY" ]; then
     mv "/data/adb/modules/zapret/$BINARY" "/data/adb/modules/zapret/nfqws"
     chmod 777 "/data/adb/modules/zapret/nfqws"

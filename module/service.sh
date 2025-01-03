8bf6a967f28c9e8e578d6d23848bf3fbe473875a
senko.class <79597112+deosync@users.noreply.github.com>
2025-01-03 11:25:49 +0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,30 +1,13 @@
 #!/system/bin/sh
-arch=$(uname -m)
-case "$arch" in
-    "x86_64")
-        nfqws="nfqws-x86_64"
-        ;;
-    "i386"|"i686")
-        nfqws="nfqws-x86"
-        ;;
-    "armv7l"|"arm")
-        nfqws="nfqws-arm"
-        ;;
-    "aarch64")
-        nfqws="nfqws-aarch64"
-        ;;
-    *)
-        echo "Unknown arch: $arch"
-        exit 1
-        ;;
-esac
-BINARY=nfqws-aarch64
-if [ -f "/data/adb/modules/zapret/$BINARY" ]; then
-    mv "/data/adb/modules/zapret/$BINARY" "/data/adb/modules/zapret/nfqws"
-    chmod 777 "/data/adb/modules/zapret/nfqws"
-    rm -rf /data/adb/modules/zapret/nfqws-*
-    if [ $(stat -c "%a" "/data/adb/modules/zapret/zapret.sh") != "777" ]; then
-      chmod 777 "/data/adb/modules/zapret/zapret.sh"
+if [ ! -f "/data/adb/modules/zapret/nfqws" ]; then
+    case "$(uname -m)" in "x86_64") BINARY="nfqws-x86_64";; "i386"|"i686") BINARY="nfqws-x86";; "armv7l"|"arm") BINARY="nfqws-arm";; "aarch64") BINARY="nfqws-aarch64";; *) echo "Unknown arch: $(uname -m)"; exit 1;; esac
+    if [ -f "/data/adb/modules/zapret/$BINARY" ]; then
+        mv "/data/adb/modules/zapret/$BINARY" "/data/adb/modules/zapret/nfqws"
+        chmod 777 "/data/adb/modules/zapret/nfqws"
+        rm -rf /data/adb/modules/zapret/nfqws-*
+        if [ $(stat -c "%a" "/data/adb/modules/zapret/zapret.sh") != "777" ]; then
+          chmod 777 "/data/adb/modules/zapret/zapret.sh"
+        fi
     fi
 fi
 su -c "/data/adb/modules/zapret/zapret.sh"
